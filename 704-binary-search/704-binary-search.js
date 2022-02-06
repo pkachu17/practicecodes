87f7145403cfe414a30ac3435635b682476838e4
pkachu17 <56520319+pkachu17@users.noreply.github.com>
2022-02-06 12:47:47 -0500

    Time: 60 ms (99.49%), Space: 45.4 MB (5.26%) - LeetHub

---
+++ 704-binary-search/704-binary-search.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    var l=0;
+    var r=nums.length-1;
+    
+    
+    while(l<=r){
+        var mid=Math.round((l+r)/2);
+        if(nums[mid]==target){
+            return mid;
+        }else if(nums[mid]>target){
+            r = mid-1;
+        }else{
+            l = mid+1;
+        }
+    }
+    return -1;
+};
