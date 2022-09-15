5c449147831dadba1466e408cb0ddbe72028c0aa
pkachu17 <56520319+pkachu17@users.noreply.github.com>
2022-09-15 02:05:22 -0400

    Time: 4 ms (93.64%), Space: 43.1 MB (87.08%) - LeetHub

--- 207-course-schedule/207-course-schedule.java
+++ 207-course-schedule/207-course-schedule.java
@@ -1,5 +1,57 @@
 class Solution {
+    
+    
+    /* *********************** DFS *********************** */
+//     public boolean canFinish(int numCourses, int[][] pre) {
+//         List<List<Integer>> adj = new ArrayList<>();
+        
+//         for(int i = 0; i < numCourses; ++i)
+//         {
+//             adj.add(new ArrayList<>());
+//         }
+        
+//         for(int[] i: pre)
+//         {
+//             adj.get(i[0]).add(i[1]);
+//         }
+        
+//         int[] vis = new int[numCourses];
+//         int[] dfsvis = new int[numCourses];
+        
+//         for(int i = 0; i < numCourses; ++i)
+//         {
+//             if(vis[i] == 0)
+//                 if(!dfs(adj, i, vis, dfsvis))
+//                     return false;
+//         }
+        
+//         return true;
+//     }
+    
+//     public boolean dfs(List<List<Integer>> adj, int node, int[] vis, int[] dfsVisited)
+//     {
+//         for(int i : adj.get(node))
+//         {
+//             if(vis[i] == 0)
+//             {
+//                 vis[i] = 1;
+//                 dfsVisited[i] = 1;
+//                 if(!dfs(adj, i, vis, dfsVisited))
+//                     return false;
+//             }
+//             else if(dfsVisited[i] == 1)
+//             {
+//                 return false;
+//             }
+//         }
+//         dfsVisited[node] = 0;
+//         return true;
+//     }
+// }
+    
     public boolean canFinish(int numCourses, int[][] pre) {
+        int[] inDeg = new int[numCourses];
+        
         List<List<Integer>> adj = new ArrayList<>();
         
         for(int i = 0; i < numCourses; ++i)
@@ -12,36 +64,40 @@ class Solution {
             adj.get(i[0]).add(i[1]);
         }
         
-        int[] vis = new int[numCourses];
-        int[] dfsvis = new int[numCourses];
+        for(int i = 0; i < numCourses; ++i)
+        {
+            for(int j : adj.get(i))
+            {
+                inDeg[j]++;
+            }
+        }
+        
+        Queue<Integer> s = new LinkedList<>();
         
         for(int i = 0; i < numCourses; ++i)
         {
-            if(vis[i] == 0)
-                if(!dfs(adj, i, vis, dfsvis))
-                    return false;
+            if(inDeg[i] == 0)
+                s.add(i);
         }
         
-        return true;
-    }
-    
-    public boolean dfs(List<List<Integer>> adj, int node, int[] vis, int[] dfsVisited)
-    {
-        for(int i : adj.get(node))
+        
+        //BFS: Kahn's/TopoSort
+        
+        int count = 0;
+        
+        while(!s.isEmpty())
         {
-            if(vis[i] == 0)
+            ++count;
+            int node = s.remove();
+            
+            for(int n : adj.get(node))
             {
-                vis[i] = 1;
-                dfsVisited[i] = 1;
-                if(!dfs(adj, i, vis, dfsVisited))
-                    return false;
-            }
-            else if(dfsVisited[i] == 1)
-            {
-                return false;
+                --inDeg[n];
+                if(inDeg[n] == 0)
+                    s.add(n);
             }
         }
-        dfsVisited[node] = 0;
-        return true;
+        
+        return count == numCourses;
     }
 }
