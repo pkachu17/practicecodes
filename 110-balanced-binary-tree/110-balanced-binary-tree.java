f2f38b15e570cd6d3fd685898bd16903c57278df
pkachu17 <56520319+pkachu17@users.noreply.github.com>
2022-09-16 03:45:43 -0400

    Time: 1 ms (99.25%), Space: 42 MB (90.78%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.java
+++ 110-balanced-binary-tree/110-balanced-binary-tree.java
@@ -14,20 +14,19 @@
  * }
  */
 class Solution {
-    boolean ans  = true;
+    public boolean ans =true;
     public boolean isBalanced(TreeNode root) {
-        
         helper(root);
         return ans;
     }
-    
     public int helper(TreeNode root){
-        if(root==null || !ans) return 0;
+        if(root == null || !ans) return 0;
         
         int left = helper(root.left);
         int right = helper(root.right);
         
-        if(Math.abs(left-right)>1)  ans=false;
-        return 1+Math.max(left, right);
+        if(Math.abs(left-right)>1) ans=false;
+        
+        return Math.max(left, right) +1;
     }
 }
