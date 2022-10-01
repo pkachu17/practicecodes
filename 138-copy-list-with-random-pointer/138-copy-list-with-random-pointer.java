43b73a3ab2c70f2d930f0975cd085b9790ff9cec
pkachu17 <56520319+pkachu17@users.noreply.github.com>
2022-10-01 13:27:27 -0400

    Time: 0 ms (100.00%), Space: 41.7 MB (95.12%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.java
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.java
@@ -17,10 +17,12 @@ class Solution {
     public Node copyRandomList(Node head) {
         Node cur = head;
         HashMap<Node, Node> map = new HashMap<>();
+        
         while(cur!=null){
             map.put(cur, new Node(cur.val));
             cur = cur.next;
         }
+        
         cur = head;
         while(cur!=null){
             map.get(cur).next = map.get(cur.next);
