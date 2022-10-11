1be329df93e9d7c6dffe773ab18ccdd2a7899684
pkachu17 <56520319+pkachu17@users.noreply.github.com>
2022-10-11 12:12:01 -0400

    Time: 1 ms (100.00%), Space: 42.3 MB (95.24%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()) return false;
+        char[] str1 = s.toCharArray();
+        char[] str2 = t.toCharArray();
+        int[] arr = new int[26];
+        
+        for(int i=0; i<str1.length; i++){
+            arr[str1[i]-97]++;
+        }
+        for(int i=0; i<str2.length; i++){
+            arr[str2[i]-97]--;
+        }
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]!=0) return false;
+        }
+        return true;
+    }
+}
