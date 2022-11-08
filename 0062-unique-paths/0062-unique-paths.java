0310adea790bf3b3b8c2be6296c7f0d8c697c044
pkachu17 <56520319+pkachu17@users.noreply.github.com>
2022-11-08 18:19:54 -0500

    Time: 0 ms (100.00%), Space: 39.2 MB (88.88%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,13 +1,34 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int[] dp = new int[n];
-        Arrays.fill(dp, 1);
         
-        for(int i=1; i<m; i++){
-            for(int j=1; j<n; j++){
-                dp[j] = dp[j] + dp[j-1];
+//  USING 1D DP TC=M*N SC=N
+//         int[] dp = new int[n];
+//         Arrays.fill(dp, 1);
+        
+//         for(int i=1; i<m; i++){
+//             for(int j=1; j<n; j++){
+//                 dp[j] = dp[j] + dp[j-1];
+//             }
+//         }
+//         return dp[n-1];
+        
+        int[][] dp = new int[m][n];
+        //fill out last row
+        for(int j=0; j<n; j++){
+            dp[m-1][j] = 1;
+        }
+        //fill out last column
+        for(int i=0; i<m;i++){
+            dp[i][n-1] =1;
+        }
+        
+        for(int i=m-2; i>=0; i--){
+            for(int j=n-2; j>=0; j--){
+                dp[i][j] = dp[i+1][j] + dp[i][j+1];
             }
         }
-        return dp[n-1];
+        return dp[0][0];
+        
+        
     }
 }
