f5e3b7fe56046e5231c0c0b82d7a44fc5b9f1540
pkachu17 <56520319+pkachu17@users.noreply.github.com>
2022-11-09 14:45:54 -0500

    Time: 8 ms (65.88%), Space: 45.6 MB (34.53%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -4,7 +4,7 @@ class Solution {
         for(int i=0; i<nums.length; i++){
             int compliment = target - nums[i];
             if(map.containsKey(compliment)){
-                return new int[]{map.get(compliment), i};
+                return new int[]{map.get(compliment),i};
             }
             map.put(nums[i], i);
         }
