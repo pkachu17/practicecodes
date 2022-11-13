cf99502e7f50b513cc3d13add3b35c13f0735bf6
pkachu17 <56520319+pkachu17@users.noreply.github.com>
2022-11-13 18:34:57 -0500

    Time: 5 ms (59.06%), Space: 45 MB (46.35%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,18 +1,17 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> cur = new ArrayList<>();
-        backTrack(candidates, target, ans, cur, 0);
-        return ans;
+        List<List<Integer>>list = new ArrayList<>();
+        backTrack(candidates, target, list, new ArrayList<>(), 0);
+        return list;
     }
-    public void backTrack(int[] candidates, int target, List<List<Integer>> ans, List<Integer> cur, int index){
-        if(target == 0) ans.add(new ArrayList<>(cur));
-        else if(target < 0 || index >= candidates.length) return;
+    public void backTrack(int[] candidates, int target, List<List<Integer>>list, List<Integer> cur, int index){
+        if(target == 0) list.add(new ArrayList(cur));
+        else if (target<0 || index >= candidates.length) return;
         else{
             cur.add(candidates[index]);
-            backTrack(candidates, target-candidates[index], ans, cur, index);
-            cur.remove(cur.get(cur.size()-1));
-            backTrack(candidates, target, ans, cur, index+1);
+            backTrack(candidates, target-candidates[index], list, cur, index);
+            cur.remove(cur.size()-1);
+            backTrack(candidates, target, list, cur, index+1);
         }
     }
 }
