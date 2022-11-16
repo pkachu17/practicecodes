52196895ada4260206afb42e31a7c1edd7f04051
pkachu17 <56520319+pkachu17@users.noreply.github.com>
2022-11-16 14:40:54 -0500

    Time: 2 ms (57.76%), Space: 43.7 MB (41.33%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -13,47 +13,22 @@
  *     }
  * }
  */
-// class Solution {
-//     public List<List<Integer>> levelOrder(TreeNode root) {
-//         List<List<Integer>> res = new ArrayList();
-//         helper(root, res, 0);
-//         return res;
-//     }
-//     public void helper(TreeNode root, List<List<Integer>> res, int level){
-//         if(root == null) return;
-//         if(res.size() == level){
-//             res.add(new ArrayList<>());
-//         }
-        
-//         res.get(level).add(root.val);
-        
-//         if(root.left != null){
-//             helper(root.left, res, level+1);
-//         }
-//         if(root.right != null){
-//             helper(root.right, res, level+1);
-//         }
-//     }
-// }
-
-
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> res = new ArrayList();
-        if(root == null)
-            return res;
+        List<List<Integer>> res = new ArrayList<>();
+        if(root == null) return res;
         Queue<TreeNode> q = new LinkedList<>();
         q.add(root);
         while(!q.isEmpty()){
-            List<Integer> temp = new ArrayList();
             int size = q.size();
+            List<Integer> temp = new ArrayList<>();
             for(int i=0; i<size; i++){
                 TreeNode element = q.remove();
                 temp.add(element.val);
-                if(element.left != null){
+                if(element.left!=null){
                     q.add(element.left);
                 }
-                if(element.right != null){
+                if(element.right!=null){
                     q.add(element.right);
                 }
             }
