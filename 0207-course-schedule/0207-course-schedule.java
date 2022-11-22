28d69a170f360b92cf19ff925ba48d9136a28475
pkachu17 <56520319+pkachu17@users.noreply.github.com>
2022-11-22 13:56:05 -0500

    Time: 10 ms (68.12%), Space: 47.3 MB (71.29%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,8 +1,9 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         List<List<Integer>> adj = new ArrayList<>();
+        
         for(int i=0; i<numCourses; i++){
-            adj.add(new ArrayList<>());
+            adj.add(new ArrayList());
         }
         
         for(int[] i : prerequisites){
@@ -13,23 +14,30 @@ class Solution {
         int[] dfsVisited = new int[numCourses];
         
         for(int i=0; i<numCourses; i++){
-            if(visited[i]==0)
-                if(!dfs(adj, visited, dfsVisited, i))
+            if(visited[i] == 0){
+                if(!dfs(adj, visited, dfsVisited, i)){
                     return false;
+                }
+            }
         }
         return true;
     }
+    
     public boolean dfs(List<List<Integer>> adj, int[] visited, int[] dfsVisited, int node){
-        for(int i : adj.get(node)){
-            if(visited[i]==0){
-                visited[i]=1;
-                dfsVisited[i]=1;
-                if(!dfs(adj, visited, dfsVisited, i))
+        for(int i: adj.get(node)){
+            if(visited[i] == 0){
+                visited[i] =1;
+                dfsVisited[i] =1;
+                if(!dfs(adj, visited, dfsVisited, i)){
                     return false;
-            }else if( dfsVisited[i]==1)
-                return false;
+                }
+            }else{
+                if(dfsVisited[i] == 1){
+                    return false;
+                }
+            }
         }
-        dfsVisited[node]=0;
+        dfsVisited[node] = 0;
         return true;
     }
 }
