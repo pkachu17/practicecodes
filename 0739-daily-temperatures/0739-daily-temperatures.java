bb3b617b2a664cca3919ee666602e19ff1460513
pkachu17 <56520319+pkachu17@users.noreply.github.com>
2022-11-25 01:41:32 -0500

    Time: 184 ms (45.03%), Space: 136.1 MB (25.31%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int[] ans =  new int[temperatures.length];
+        Stack<Integer> stack =new Stack<>();
+        for(int curDay =0; curDay<temperatures.length; curDay++){
+            while(!stack.isEmpty() &&  temperatures[curDay] > temperatures[stack.peek()] ){
+                int prevDay = stack.pop();
+                ans[prevDay] = curDay - prevDay;
+            }
+            stack.add(curDay);
+        }
+        return ans;
+    }
+}
