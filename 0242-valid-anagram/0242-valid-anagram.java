236ad87dd2efcb75a7c79e00d8c0836c28a69bdb
pkachu17 <56520319+pkachu17@users.noreply.github.com>
2023-02-12 14:18:08 -0500

    Time: 2 ms (95.55%), Space: 42.5 MB (66.68%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,18 +1,31 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        char[] str1 = s.toCharArray();
-        char[] str2 = t.toCharArray();
-        int[] arr = new int[26];
         
-        for(int i=0; i<str1.length; i++){
-            arr[str1[i]-97]++;
+        s = Sort(s);
+        t = Sort(t);
+        if(s.equals(t)){
+            return true;
         }
-        for(int i=0; i<str2.length; i++){
-            arr[str2[i]-97]--;
-        }
-        for(int i=0; i<arr.length; i++){
-            if(arr[i]!=0) return false;
-        }
-        return true;
+        return false;
+        
+//         char[] str1 = s.toCharArray();
+//         char[] str2 = t.toCharArray();
+//         int[] arr = new int[26];
+        
+//         for(int i=0; i<str1.length; i++){
+//             arr[str1[i]-97]++;
+//         }
+//         for(int i=0; i<str2.length; i++){
+//             arr[str2[i]-97]--;
+//         }
+//         for(int i=0; i<arr.length; i++){
+//             if(arr[i]!=0) return false;
+//         }
+//         return true;
+    }
+    public String Sort(String s){
+    char[] c = s.toCharArray();
+    Arrays.sort(c);
+    return new String(c);
     }
 }
