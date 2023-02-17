9e3bcf14d1099052214fc38bcba8223b99184b20
pkachu17 <56520319+pkachu17@users.noreply.github.com>
2023-02-16 20:58:38 -0500

    Time: 1003 ms (9.61%), Space: 42.9 MB (11.51%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,16 +1,14 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        s1 = sort(s1);
+        s1 = Sort(s1);
         for(int i=0; i<=s2.length()-s1.length(); i++){
-            if(s1.equals(sort(s2.substring(i, i+s1.length())))) return true;
-         }
+            if(s1.equals(Sort(s2.substring(i, i+s1.length())))) return true;
+        }
         return false;
     }
-    
-    public String sort(String s){
-        char[] c = s.toCharArray();
-        Arrays.sort(c);
-        return new String(c);
-            
+    public String Sort(String s){
+        char[] ca = s.toCharArray();
+        Arrays.sort(ca);
+        return new String(ca);
     }
 }
