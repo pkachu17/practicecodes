9a7d35fa785e1d4346a00bfdf4cdd1dfb3d1da7c
pkachu17 <56520319+pkachu17@users.noreply.github.com>
2023-02-20 18:38:06 -0500

    Time: 15 ms (33.91%), Space: 42.6 MB (48.79%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,14 +1,13 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        HashSet<String> seen = new HashSet();
-        for(int i =0; i<9; i++){
+        HashSet<String> seen = new HashSet<>();
+        for(int i=0; i<9; i++){
             for(int j=0; j<9; j++){
                 char number = board[i][j];
                 if(number != '.'){
-                    if(!seen.add(number+"in row"+ i) ||
-                      !seen.add(number+"in col"+ j) ||
-                      !seen.add(number+"in block"+ i/3 + j/3))
-                        return false;
+                    if(!seen.add(number+"in row"+  i) ||
+                        ! seen.add(number+"in col"+ j) ||
+                      ! seen.add(number+"in block" + i/3 + j/3)) return false;
                 }
             }
         }
