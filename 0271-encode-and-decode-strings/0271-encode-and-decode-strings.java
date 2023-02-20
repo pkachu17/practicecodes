e781de98ccb0cacbdfa1846bf33a20b9e8a55fe9
pkachu17 <56520319+pkachu17@users.noreply.github.com>
2023-02-20 18:51:45 -0500

    Time: 4 ms (98.31%), Space: 43.3 MB (44.62%) - LeetHub

--- 0271-encode-and-decode-strings/0271-encode-and-decode-strings.java
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.java
@@ -2,20 +2,21 @@ public class Codec {
 
     // Encodes a list of strings to a single string.
     public String encode(List<String> strs) {
-        StringBuilder s = new StringBuilder();
-        for(String str : strs)
-            s.append(str.length()).append('#').append(str);
-        return s.toString();
+        StringBuilder sb = new StringBuilder();
+        for(String str : strs){
+            sb.append(str.length()).append('#').append(str);
+        }
+        return sb.toString();
     }
 
     // Decodes a single string to a list of strings.
     public List<String> decode(String s) {
         ArrayList<String> list = new ArrayList<>();
         int i=0;
-        while(i<s.length()){
+        while(i < s.length()){
             int j=i;
             while(s.charAt(j)!='#') j++;
-            int length = Integer.valueOf(s.substring(i,j));
+            int length = Integer.parseInt(s.substring(i,j));
             i = j+1+length;
             list.add(s.substring(j+1, i));
         }
