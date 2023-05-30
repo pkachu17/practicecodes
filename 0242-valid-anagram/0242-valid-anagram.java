9e2bb84048a22552fff47e45fe90ce7d16c44c25
pkachu17 <56520319+pkachu17@users.noreply.github.com>
2023-05-30 15:29:48 -0400

    Time: 2 ms (99.75%), Space: 44 MB (7.04%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -15,13 +15,41 @@ class Solution {
         for(int i=0; i<str1.length; i++){
             arr[str1[i]-97]++;
         }
+        
         for(int i=0; i<str2.length; i++){
             arr[str2[i]-97]--;
         }
+        
         for(int i=0; i<arr.length; i++){
-            if(arr[i]!=0) return false;
+            if(arr[i] != 0) return false;
         }
+        
         return true;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+//         char[] str1 = s.toCharArray();
+//         char[] str2 = t.toCharArray();
+//         int[] arr = new int[26];
+        
+//         for(int i=0; i<str1.length; i++){
+//             arr[str1[i]-97]++;
+//         }
+//         for(int i=0; i<str2.length; i++){
+//             arr[str2[i]-97]--;
+//         }
+//         for(int i=0; i<arr.length; i++){
+//             if(arr[i]!=0) return false;
+//         }
+//         return true;
         
     }
     public String Sort(String s){
